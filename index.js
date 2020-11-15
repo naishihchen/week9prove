58c889930270d5900c0381ae64e93a27e74a1b9f
Nai Shih Chen <che17030@byui.edu>
2020-11-14 18:27:43 -0700

    Cleared page for reuse

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  function calculateRate() {
+	let mailWeight = document.getElementById("mail-weight").value;
+	var mailType = undefined;
+	var radios = document.getElementsByTagName('input');
+
+	for(i = 0; i < ele.length; i++) { 
+                  
+        if(ele[i].type="radio") { 
+                
+            if(ele[i].checked) {
+                mailType = ele[i].value; 
+            }
+        } 
+    }
+
+    console.log(mailWeight);
+    console.log(mailType); 
+}
