8d962330ee60717b550bd0d532ea7acd7488d5bf
Nai Shih Chen <che17030@byui.edu>
2020-11-14 21:56:38 -0700

    Created working function, attempting to make ejs deployment

--- index.js
+++ index.js
@@ -13,12 +13,86 @@ express()
   .get('/getRates', (req, res) => res.render('pages/getRates'))
   .post('/postRates', (req, res) => {
   	console.log(req.body);
-  	var formInputs = {
-  			mailWeight: req.body.mailweight,
-  			mailType: req.body.mailtypes
-  		}
+
+  	let mailWeight = req.body.mailweight;
+    var mailType = req.body.mailtypes;
+
+    var baseRate = 0.00;
+    var weightRate = 0.00;
+    var finalPrice = 0.00;
+
+    var output = "";
+
+    console.log(mailWeight);
+    console.log(mailType); 
+
+    switch(mailType) {
+      case 'letters-stamped':
+        
+        baseRate = 0.40;
+
+        if (mailWeight < 4) {
+          weightRate = 0.15 * mailWeight;
+        } else {
+          weightRate = 0.15 * 4;
+        }
+
+        finalPrice = baseRate + weightRate;
+
+        output = "The final price of your parcel is $" + finalPrice.toFixed(2);
+
+        break;
+      case 'letters-metered':
+
+        baseRate = 0.35;
+
+        if (mailWeight < 4) {
+          weightRate = 0.15 * mailWeight;
+        } else {
+          weightRate = 0.15 * 4;
+        }
+        
+        finalPrice = baseRate + weightRate;
+
+        output = "The final price of your parcel is $" + finalPrice.toFixed(2);
+        break;
+      case 'large-envelopes-flat':
+
+        baseRate = 0.80;
+
+        if (mailWeight < 13) {
+          weightRate = 0.20 * mailWeight;
+        } else {
+          weightRate = 0.20 * 13;
+        }
+        
+        finalPrice = baseRate + weightRate;
+
+        output = "The final price of your parcel is $" + finalPrice.toFixed(2);
+
+        break;
+      case 'first-class-retail':
+
+        if (mailWeight < 5) {
+          finalPrice = 3.80;
+        } else if (mailWeight < 9) {
+          finalPrice = 4.60;
+        } else if (mailWeight < 13) {
+          finalPrice = 5.3;
+        } else {
+          finalPrice = 5.9;
+        }
+
+        output = "The final price of your parcel is $" + finalPrice.toFixed(2);
+
+        break;
+      default:
+        output = "Sorry, an error has occurred. Please consult the error logs.";
+        break;
+    }
+
   	res.render('pages/postRates', {
-  		data: formInputs
+  		data: output
   	})
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
