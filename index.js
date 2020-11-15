09a6aaf86bdcc177c193d4dbce41c3b2feb3fb0e
Nai Shih Chen <che17030@byui.edu>
2020-11-14 21:25:33 -0700

    Created working function, attempting to make ejs deployment

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+var parser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/getRates', (req, res) => res.render('pages/getRates.ejs'))
+  .get('/getRates', (req, res) => res.render('pages/getRates'))
+  .post('/postRates', (req, res) => {
+  	var formInputs = {
+  			mailWeight: req.body.mail-weight;
+  			mailType: req.body.mail-types;
+  		}
+  	res.render('pages/postRates', {
+  		data: formInputs;
+  	})
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
